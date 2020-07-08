dd7a4c298b31054578fedd73f6b87383dca64f2e
Conner Petzold <cpetzold@gmail.com>
2020-07-07 17:38:14 -0700

    Roping

--- Player.gd
+++ Player.gd
@@ -9,25 +9,71 @@ enum PlayerState {
 export var gravity = 10
 export var move_speed = 10
 export var ground_drag = 0.8
-export var air_drag = 0.8
+export var air_drag = 1
+export var tethered_move_speed = 10
+export var tethered_drag = 0.99
+export var untethered_drag = 1
 export var jump_force = -500
 export var velocity_input_threshold = Vector2(300, 300)
 
 var acceleration = Vector2()
 var velocity = Vector2()
-var state = PlayerState.FALLING
+var state = PlayerState.TETHERED
 
 onready var sprite = $Sprite
+onready var rope = $Rope
 
 func _ready():
 	rset_config("velocity", MultiplayerAPI.RPC_MODE_REMOTE)
 	rset_config("position", MultiplayerAPI.RPC_MODE_REMOTE)
+	
+	var space_state = get_world_2d().direct_space_state
+	# use global coordinates, not local to node
+	var rope_v = Vector2(cos(deg2rad(-15)), sin(deg2rad(-15))) * 1000
+	var result = space_state.intersect_ray(position, position + rope_v, [get_rid()])
+	
+	rope.length = (position - result.position).length()
+	rope.push(result.position)
 
-func _normal_movement(drag):
+func _tethered_movement(delta):
+	var move_direction = _get_move_direction()
+	var to_pivot = position - rope.pivot()
+	var tangent = to_pivot.tangent().normalized()
+	
+	acceleration = Vector2.DOWN * gravity
+	
+	if move_direction.x != 0:
+		acceleration += Vector2(move_direction.x * tethered_move_speed, tethered_move_speed)
+		
+	if move_direction.y != 0:
+		rope.length += move_direction.y * 5
+		acceleration -= to_pivot.normalized() * move_direction.y * tethered_move_speed
+	
+	velocity += acceleration
+	
+	velocity = tangent * velocity.dot(tangent) * tethered_drag
+	
+	var new_position = position + (velocity * delta)
+	var new_to_pivot = new_position - rope.pivot()
+	var new_length_to_pivot = new_to_pivot.length()
+	new_position = rope.pivot() + (new_to_pivot * (rope.length / new_length_to_pivot))
+	
+	var move_result = move_and_collide(new_position - position)
+	
+	if move_result:
+		velocity = velocity.bounce(move_result.normal)
+		
+	rope.length = (position - rope.pivot()).length()
+	
+	if Input.is_action_just_pressed("toggle_rope"):
+		rope.hide()
+		state = PlayerState.FALLING
+
+func _normal_movement(delta, drag):
 	var apply_drag = false
 	acceleration = Vector2(0, gravity)
 	
-	if is_network_master():
+	if true || is_network_master():
 		var move_direction = _get_move_direction()
 		var move_force = move_direction * move_speed
 		
@@ -57,23 +103,25 @@ func _next_state(from_state):
 				return PlayerState.GROUNDED
 	return from_state
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	state = _next_state(state)
 
 	match state:
 		PlayerState.GROUNDED:
-			_normal_movement(ground_drag)
+			_normal_movement(delta, ground_drag)
 			if Input.is_action_just_pressed("jump"):
 				_jump()
 		PlayerState.FALLING:
-			_normal_movement(air_drag)
+			_normal_movement(delta, air_drag)
+		PlayerState.TETHERED:
+			_tethered_movement(delta)
 
 	if velocity.x != 0:
 		sprite.flip_h = velocity.x < 0
 
-	if is_network_master():
-		rset("position", position)
-		rset("velocity", velocity)
+	#if is_network_master():
+	#	rset("position", position)
+	#	rset("velocity", velocity)
 
 
 func _jump():
@@ -82,5 +130,5 @@ func _jump():
 func _get_move_direction() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		0
+		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
---
+++ Rope.gd
@@ -0,0 +1,27 @@
+extends Line2D
+
+export var global_points = PoolVector2Array()
+export var length = 100
+
+func _ready():
+	_update_points()
+
+func _process(delta):
+	_update_points()
+	
+func _update_points():
+	clear_points()
+	for p in global_points:
+		add_point(to_local(p))
+	add_point(position)
+
+func push(position):
+	var result = global_points.push_back(position)
+	_update_points()
+
+func pop():
+	global_points.remove(global_points.size() - 1)
+	_update_points()
+
+func pivot():
+	return global_points[global_points.size() - 1]
