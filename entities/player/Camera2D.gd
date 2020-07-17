4617ad4ca86ca6b3d0d822601127d7bffc3c8008
Conner Petzold <cpetzold@gmail.com>
2020-07-17 00:58:10 -0700

    Add camera bounds

---
+++ entities/player/Camera2D.gd
@@ -0,0 +1,21 @@
+extends Camera2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var tm: TileMap = get_tree().get_root().get_node("Game/Level/TileMap")
+	var rect = tm.get_used_rect()
+	
+	var pos = tm.map_to_world(rect.position)
+	var end = tm.map_to_world(rect.end)
+	
+	limit_top = pos.y
+	limit_left = pos.x
+	limit_bottom = end.y
+	limit_right = end.x
+	
