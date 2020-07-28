c919bac4db1b9a69da20cfdebd9550f0ad33df77
Conner Petzold <cpetzold@gmail.com>
2020-07-27 20:00:38 -0700

    Fix builds

--- SceneChanger.gd
+++ SceneChanger.gd
@@ -9,7 +9,7 @@ func change_scene(path, delay = 0.5):
 	yield(get_tree().create_timer(delay), "timeout")
 	animation_player.play("fade")
 	yield(animation_player, "animation_finished")
-	assert(get_tree().change_scene(path) == OK)
+	get_tree().change_scene(path)
 	animation_player.play_backwards("fade")
 	yield(animation_player, "animation_finished")
 	emit_signal("scene_changed")
