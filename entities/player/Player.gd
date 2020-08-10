6945be920380aa78e26bad4b87c0734092128bc3
squiffel <qytiswork@gmail.com>
2020-08-09 20:42:18 -0400

    Deleted old sprites + Added new input key

--- entities/player/Player.gd
+++ entities/player/Player.gd
@@ -125,6 +125,7 @@ func input_direction() -> Vector2:
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
 
-func change_character(rig_textures_path):
-	var t = load(rig_textures_path)
-	rig.textures = t
+#Not in use
+#func change_character(rig_textures_path):
+	#var t = load(rig_textures_path)
+	#rig.textures = t
